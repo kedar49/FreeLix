97a6f0dc94a03f20d07fb10f37944440509e9641
Kedar <kedarsathe49@gmail.com>
2024-07-26 22:51:34 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+#FreeLix
+
+This is React & tailwind css web-app for Transcription and translation Backend with LLM models.
+
+I have Used Open Source model from HUggingface for Translation & Translation : openai/whisper-large-v3
+
+
+ # Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("automatic-speech-recognition", model="openai/whisper-large-v3") 
+
+
+to run this 
+
+clone this repo
---
+++ src/utils/presets.js
@@ -0,0 +1,52 @@
+export const MessageTypes = {
+    DOWNLOADING: "DOWNLOADING",
+    LOADING: "LOADING",
+    RESULT: "RESULT",
+    RESULT_PARTIAL: "RESULT_PARTIAL",
+    INFERENCE_REQUEST: "INFERENCE_REQUEST",
+    INFERENCE_DONE: "INFERENCE_DONE",
+};
+
+export const LoadingStatus = {
+    SUCCESS: "success",
+    ERROR: "error",
+    LOADING: "loading",
+};
+
+export const ModelNames = {
+    WHISPER_LARGE_EN: "openai/whisper-large-v3",
+   
+};
+
+export const LANGUAGES = {
+    "English": "eng_Latn",
+    "Mandarin Chinese (Simplified)": "zho_Hans",
+    "Hindi": "hin_Deva",
+    "Spanish": "spa_Latn",
+    "French": "fra_Latn",
+    "Arabic": "arb_Arab",
+    "Bengali": "ben_Beng",
+    "Russian": "rus_Cyrl",
+    "Portuguese": "por_Latn",
+    "Urdu": "urd_Arab",
+    "Indonesian": "ind_Latn",
+    "German": "deu_Latn",
+    "Japanese": "jpn_Jpan",
+    "Swahili": "swh_Latn",
+    "Marathi": "mar_Deva",
+    "Telugu": "tel_Telu",
+    "Turkish": "tur_Latn",
+    "Korean": "kor_Hang",
+    "Tamil": "tam_Taml",
+    "Italian": "ita_Latn",
+    "Thai": "tha_Thai",
+    "Gujarati": "guj_Gujr",
+    "Kannada": "kan_Knda",
+    "Persian": "pes_Arab",
+    "Polish": "pol_Latn",
+    "Dutch": "nld_Latn",
+    "Ukrainian": "ukr_Cyrl",
+    "Malay": "zsm_Latn",
+    "Vietnamese": "vie_Latn",
+    "Hausa": "hau_Latn",
+};
